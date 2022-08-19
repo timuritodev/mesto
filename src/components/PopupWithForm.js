7ed46e196673bf594d0ec0b7112cd2210b069bcb
luciD <sosalnet@yandex.ru>
2022-08-19 14:13:44 +0300

    done UserInfo PopupwithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,31 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, handleFormSubmit) {
+        super(popupSelector);
+        this._form = this._popup.querySelector('.popup__form');
+        this._inputs = [...this._form.querySelector('.popup__input')]
+        this._values = {};
+        this._handleFormSubmit = handleFormSubmit;
+    }
+
+    _getInputValues() {
+        this._inputs.forEach((input) => {
+            this._values[input.name] = input.value;
+        });
+        return this._values;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this._form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this._handleFormSubmit(this._getInputValues());
+        });
+    }
+
+    close() {
+        super.close();
+        this._form.reset();
+    }
+}
---
+++ src/components/UserInfo.js
@@ -0,0 +1,18 @@
+export default class Userinfo {
+    constructor({ usernameSelector, descriptionSelector }) {
+        this._username = document.querySelector(usernameSelector);
+        this._description = document.querySelector(descriptionSelector);
+    }
+
+    getUserInfo() {
+        return {
+            username: this._username.textContent,
+            description: this._description.textContent
+        };
+    }
+
+    setUserInfo(username, description) {
+        this._username.textContent = username;
+        this._description.textContent = description;
+    }
+}
